e90c3476249671823cbc1005601dab8e60049e7f
TusharGoyal <tushargoyal.cs@gmail.com>
2026-05-12 22:12:04 +0530

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
     <>
       <div className="header">
         <a href="/#" className="navbar-title" data-cursor="disable">
-          AM
+          TG
         </a>
         <a
           href="https://www.linkedin.com/in/tushgoyal/"
